b8cfd6accf0c76c2d918ce07ad42fd30f36bf008
devkmj <kimjiyeonn0628@gmail.com>
2025-05-08 18:29:53 +0900

    feat :  Redis 캐시 설정 추가

--- src/main/java/kr/hhplus/be/server/ServerApplication.java
+++ src/main/java/kr/hhplus/be/server/ServerApplication.java
@@ -4,17 +4,13 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import javax.sql.DataSource;
-import java.sql.Connection;
 
 @SpringBootApplication
+@EnableCaching
 @EnableJpaRepositories(basePackages = "kr.hhplus.be.server.infrastructure")
 @EntityScan(basePackages = "kr.hhplus.be.server.domain")
 public class ServerApplication {
