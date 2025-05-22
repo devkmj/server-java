7d138e31b7c4949797fb1215ce6fd1438179f338
devkmj <kimjiyeonn0628@gmail.com>
2025-05-22 18:21:24 +0900

    refactor : 인기상품 랭킹 패키지 구조 변경

---
+++ src/main/java/kr/hhplus/be/server/interfaces/api/ranking/event/WeeklyRankingAsyncUpdater.java
@@ -0,0 +1,22 @@
+package kr.hhplus.be.server.interfaces.api.ranking.event;
+
+import kr.hhplus.be.server.application.ranking.port.WeeklyRankingRebuilder;
+import kr.hhplus.be.server.domain.ranking.event.RankingUpdatedEvent;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class WeeklyRankingAsyncUpdater {
+
+    private final WeeklyRankingRebuilder weeklyRankingRebuilder;
+
+    @Async
+    @EventListener(condition = "#event.period == PeriodType.DAILY")
+    public void rebuildWeekly(RankingUpdatedEvent event) {
+        weeklyRankingRebuilder.rebuildThisWeek();
+    }
+    
+}
