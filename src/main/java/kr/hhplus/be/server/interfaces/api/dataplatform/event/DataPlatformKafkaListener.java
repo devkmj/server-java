c85428cd772019c7057b8f8bd6454adf9388954e
devkmj <kimjiyeonn0628@gmail.com>
2025-05-29 16:59:27 +0900

    feat(dataplatform): 데이터 플랫폼 전송을 Kafka로 구현 및 테스트코드 작성

--- src/main/java/kr/hhplus/be/server/interfaces/api/dataplatform/event/DataPlatformKafkaListener.java
+++ src/main/java/kr/hhplus/be/server/interfaces/api/dataplatform/event/DataPlatformKafkaListener.java
@@ -1,25 +1,22 @@
 package kr.hhplus.be.server.interfaces.api.dataplatform.event;
 
-import kr.hhplus.be.server.domain.dataplatform.event.DataPlatformEventPublisher;
 import kr.hhplus.be.server.domain.order.event.OrderConfirmedEvent;
+import kr.hhplus.be.server.infrastructure.dataplatform.RestDataPlatformClient;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
 
-@Slf4j
 @Component
 @RequiredArgsConstructor
-public class DataPlatformOrderEventListener {
+public class DataPlatformKafkaListener {
 
-    private final DataPlatformEventPublisher dataPlatformEventPublisher;
-
-    @Async
-    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    private final RestDataPlatformClient restDataPlatformClient;
+    @KafkaListener(topics = "data_platform_order_events", groupId = "data_platform")
     public void onOrderConfirmedEvent(OrderConfirmedEvent event) {
-        log.info("[DataPlatformOrderEventListener] 수신된 이벤트: {}", event);
-        dataPlatformEventPublisher.publish(event);
+        restDataPlatformClient.sendOrderConfirmed(event.getOrderId());
     }
+
 }
